52186b691baee1eae48b9f99aae890355c51965a
Dombovári Balázs <45357979+dombovari-balazs@users.noreply.github.com>
2026-02-02 17:47:46 +0100

    shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,18 @@
 
 
 fn main() {
-    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-    let mut x = 5;
+    let x = 5;
     println!("x = {x}");
-    x = 10;
+
+    let x = x + 1;
     println!("x = {x}");
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+
+    println!("The value of x is: {x}");
+    
 }
